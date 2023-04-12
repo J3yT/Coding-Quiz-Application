413fc273d03b93702b6acd732bbb69962a64c0ad
Jason Tavera <taverajason7@gmail.com>
2023-04-11 21:53:50 -0500

    added readme and completed it

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Coding-Quiz-Application
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+This coding quiz copy allowed me to improve upon my understanding of java script. Also to further improve my knowledge in coding interview simulations. This application allows the user to take a quiz and be penalized for each wrong answer until the time runs out. 
+
+## Usage
+
+
+## Credits
--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 
 let quizDiv = document.querySelector("#quiz")
 let questionButton1 = document.querySelector("#answer1")
+let questionButton2 = document.querySelector("#answer2")
+let questionButton3 = document.querySelector("#answer3")
+let questionButton4 = document.querySelector("#answer4")
 
-let timer = 90
 
-let timerId = setInterval(function(){
+// let timer = 90
 
-    timer -=1
-    console.log(timer)
-}, 1000)
+// let timerId = setInterval(function(){
+
+//     timer -=1
+//     console.log(timer)
+
+// }, 1000)
 
 
 
 
 
 let questions = [{question: "Which pokemon does Ash get at the start of pokemon?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 3 },
+{question: "Which pokemon is able to deal fire damage?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 2 },
+{question: "Which pokemon is able to deal fire damage?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 2 },
+{question: "Which pokemon is able to deal fire damage?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 2 },
+{question: "Which pokemon is able to deal fire damage?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 2 },
 {question: "Which pokemon is able to deal fire damage?", answers:["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: 2 }]
 
 
@@ -23,8 +32,11 @@ let currentQuestion = 0
 renderQuestion();
 
 function renderQuestion(){
-    console.log(questions[currentQuestion].question)
+    (questions[currentQuestion].question)
     questionButton1.textContent = questions[currentQuestion].answers[0];
+    questionButton2.textContent = questions[currentQuestion].answers[1];
+    questionButton3.textContent = questions[currentQuestion].answers[2];
+    questionButton4.textContent = questions[currentQuestion].answers[3];
     console.log(questions[0].answers[1])
     console.log(questions[0].answers[2])
     console.log(questions[0].answers[3])
